1490b9dc466f283345704183dc7ad44affdcbe94
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-29 08:16:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [weatherData, setWeatherData] = useState({})
+  useEffect(()=>{
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Delhi&units=imperial&appid=895284fb2d2c50a520ea537456963d9c')           
+     .then(response => response.json())
+     .then(data => console.log(data));
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
